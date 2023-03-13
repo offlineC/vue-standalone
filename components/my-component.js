48caf171825b63fc6176547869a5dca7bfe42a57
offlineC <rochaleanne@gmail.com>
2023-03-13 12:13:38 +0800

    updated component

--- components/my-component.js
+++ components/my-component.js
@@ -1,6 +1,17 @@
+// //for displaying static data
+// export default {
+//   data() {
+//     return { count: 0 }
+//   },
+//   template: `<div>count is {{ count }}</div>`
+// }
+
+//adding click events
 export default {
-  data() {
-    return { count: 0 }
-  },
-  template: `<div>count is {{ count }}</div>`
+    data() {
+        return {
+            count: 0
+        }
+    },
+    template: '<div @click="count++">count is {{count}}</div>'
 }
